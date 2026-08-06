e586e2244a273ff764ffc09f4b9736d3075c3484
mhk61366-svg <mhk61366@gmail.com>
2026-08-06 06:33:35 -0700

    venv updated with additional packages and readded to gitignore

--- service.py
+++ service.py
@@ -3,17 +3,27 @@ class UserService:
         self.repo = repository
 
     def get_all_users(self):
-        return self.repo.get_all_users()
+        users = self.repo.get_all_users()
+        return {"message": "Users retrieved successfully", "data": users}
 
     def get_user_by_id(self, user_id: int):
-        return self.repo.get_user_by_id(user_id)
+        user = self.repo.get_user_by_id(user_id)
+        if not user:
+            return None
+        return {"message": "User retrieved successfully", "data": user}
 
     def create_user(self, user_data: dict):
-        return self.repo.create_user(user_data)
+        user = self.repo.create_user(user_data)
+        return {"message": "User created successfully", "data": user}
 
     def update_user(self, user_id: int, user_data: dict):
-        return self.repo.update_user(user_id, user_data)
-    
+        user = self.repo.update_user(user_id, user_data)
+        if not user:
+            return None
+        return {"message": "User updated successfully", "data": user}
+
     def delete_user(self, user_id: int):
-        return self.repo.delete_user(user_id)
-    
+        deleted = self.repo.delete_user(user_id)
+        if not deleted:
+            return None
+        return {"message": "User deleted successfully"}
